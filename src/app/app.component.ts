844cae532e20507783fad24d0f2bf03d781e3134
Sean Lemster <sean.lemster@noa.nintendo.com>
2018-03-21 10:32:05 -0700

    Add bootstrap styling for Decks and Cards Add dragula functionality to cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css', '../../node_modules/dragula/dist/dragula.css']
 })
 export class AppComponent {
   title = 'app';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
+import { NgModule }      from '@angular/core';
+import { AppComponent }  from './app.component';
+import { DragulaModule } from '../../node_modules/ng2-dragula';
 
 
 @NgModule({
@@ -10,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragulaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
